a680d2cc7e49299b0ad9f5ec01fe15e1db95771d
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-08-19 10:58:57 -0500

    a3

--- function.jsx
+++ function.jsx
@@ -21,3 +21,19 @@ function SearchBar({ filterText, inStockOnly }) {
     </form>
   );
 }
+
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+    </div>
+  );
+}
