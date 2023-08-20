e6d6f802f87b76e0c2814c102eb6cf049aacf8ec
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-08-20 16:30:07 -0500

    a4

--- addReact.js/index.js
+++ addReact.js/index.js
@@ -15,5 +15,5 @@ import { createRoot } from 'react-dom/client';
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
